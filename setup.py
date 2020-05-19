639d5c28e830950ec8879c8885ba8ffbf9b8a1c5
Casper Kaandorp <c.s.kaandorp@uu.nl>
2020-05-19 17:13:07 +0200

    uu_anonymize

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
 
-setup(name='anonymize',
-      version='0.1',
-      description='',
-      url='https://github.com/cskaandorp/anonymize',
-      author='C.S. Kaandorp',
-      author_email='c.s.kaandorp@uu.nl',
-      license='MIT',
-      packages=['anonymize'],
-      zip_safe=False
+setup(
+    name='uu_anonymize',
+    version='0.1',
+    description='',
+    url='https://github.com/cskaandorp/anonymize',
+    author='C.S. Kaandorp',
+    author_email='c.s.kaandorp@uu.nl',
+    license='MIT',
+    packages=['anonymize'],
+    zip_safe=False
 )
