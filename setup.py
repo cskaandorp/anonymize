28a09f7c0fd1a93d4698fd1d07f755c6ebff2749
Casper Kaandorp <c.s.kaandorp@uu.nl>
2020-05-19 16:56:00 +0200

    init

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='anonymize',
+      version='0.1',
+      description='',
+      url='https://github.com/cskaandorp/anonymize',
+      author='C.S. Kaandorp',
+      author_email='c.s.kaandorp@uu.nl',
+      license='MIT',
+      packages=['anonymize'],
+      zip_safe=False
+)
