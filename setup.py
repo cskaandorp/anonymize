503144f5a6b6759fb95787a42e0c4a3c1c8ddd38
Casper Kaandorp <c.s.kaandorp@uu.nl>
2020-05-22 16:45:45 +0200

    version 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='anonymize_UU',
-    version='0.1',
+    version='0.1.1',
     description='',
     url='https://github.com/cskaandorp/anonymize',
     author='C.S. Kaandorp',
